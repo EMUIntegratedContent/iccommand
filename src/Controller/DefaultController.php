1046ff6219b4b0bf07dd9434f23e75df19f5bd69
Chris Puzzuoli <cpuzzuol@emich.edu>
2018-04-06 17:17:02 -0400

    playing around with unit and function testing

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
 
 class DefaultController extends Controller
 {
@@ -17,5 +19,13 @@ class DefaultController extends Controller
         return $this->render('index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
+    }
+
+    /**
+     * @Route("/unittest", name="unit_test")
+     */
+    public function unitTest()
+    {
+      return new Response('Hello World', 201, array('Content-Type' => 'application/json; charset=UTF-8'));
     }
 }
