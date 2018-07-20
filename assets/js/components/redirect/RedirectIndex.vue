dde5166ab93054f35b18e19e48c9256452dec2a5
JR <rvictori@emich.edu>
2018-07-20 12:07:10 -0400

    New Redirects Index Page

--- assets/js/components/redirect/RedirectIndex.vue
+++ assets/js/components/redirect/RedirectIndex.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
-    <heading>
-      <span slot="title">Redirects</span>
-    </heading>
-    <p>The best app of the three applications.</p>
+  <div class="row">
+    <div class="col">
+      <div class="app-hero">
+        <h1 style="color: #ffffff; stroke: #000000">Redirects Application</h1>
+        <img class="img-fluid" alt="Redirects Application" src="/images/redirect-front.png"/>
+      </div>
+      <p>This application stores all information pertaining to the URL redirections of broken links and vanity links. Users with full access of this app have the ability to modify, add, or delete redirects.</p>
+    </div>
   </div>
 </template>
 
