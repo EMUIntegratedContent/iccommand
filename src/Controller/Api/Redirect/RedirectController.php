4541037ff9a6c8b4c2eda09651867d145b0913ca
Chris Puzzuoli <cpuzzuol@emich.edu>
2018-08-28 15:45:12 -0400

    no longer returning whole objects on most redirect API calls

--- src/Controller/Api/Redirect/RedirectController.php
+++ src/Controller/Api/Redirect/RedirectController.php
@@ -57,10 +57,9 @@ class RedirectController extends FOSRestController
         // $context = new SerializationContext();
         // $context->setSerializeNull(true);
 
-        $serializer = $this->container->get('jms_serializer');
+        //$serializer = $this->container->get('jms_serializer');
         //$serialized = $serializer->serialize($redirect, 'json', $context);
-        $serialized = $serializer->serialize($redirect, 'json');
-        $response = new Response($serialized, 200, array('Content-Type' => 'application/json'));
+        $response = new Response($redirect->toLink, 200, array('Content-Type' => 'application/json'));
 
         return $response;
     }
