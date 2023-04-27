3a6c2485b34f152d2bb19820ea880de6bda35d14
Chris Puzzuoli <cpuzzuol@gmail.com>
2023-04-26 21:48:01 -0400

    updates to cors

--- docker_postscript.sh
+++ docker_postscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Change ownership
-chown -R www-data:www-data /var/www/html/var
+chown -R www-data:www-data /var/www/html/var /var/www/html/public
 
 # Change permissions
-chmod -R 775 /var/www/html/var
+chmod -R 775 /var/www/html/var /var/www/html/public
 
 # Start Apache in the foreground. This takes the place of "CMD ["apache2-foreground"]" in the Dockerfile!
 exec apache2-foreground
