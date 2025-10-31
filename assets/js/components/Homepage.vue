dd4f89f1158165fcc232abecb599a8d3dd751391
Chris Puzzuoli <cpuzzuol@umich.edu>
2025-10-31 15:51:21 -0400

    update front page app display

--- assets/js/components/Homepage.vue
+++ assets/js/components/Homepage.vue
@@ -7,25 +7,17 @@
 				</div>
 			</div>
 			<div class="row">
-				<template v-for="(module, i) in userModules">
-					<div
-						v-if="module.display"
-						class="card col-sm-6 col-md-4"
-						:key="'mod-' + i"
-            :class="i === 'emergency' ? 'emergency-card' : ''"
-					>
-						<div class="card-body">
-							<h5 class="card-title">{{ module.title }}</h5>
-							<p class="card-text"><span v-html="module.description"></span></p>
-							<a :href="module.buttonLink" class="btn btn-primary">{{
-								module.buttonText
-							}}</a>
-						</div>
-					</div>
-				</template>
-				<p v-if="!userHasModules">
-					You do not currently belong to any applications.
-				</p>
+        <div class="col-xs-12">
+          <p v-if="!userHasModules">
+            You do not currently belong to any applications.
+          </p>
+          <template v-else>
+            <ModuleCategoryCards v-if="campusSafetyModules.length > 0" :modules="campusSafetyModules" mdDisplay="col-md-6"></ModuleCategoryCards>
+            <ModuleCategoryCards v-if="mapAndDirectoryModules.length > 0" :modules="mapAndDirectoryModules"></ModuleCategoryCards>
+            <ModuleCategoryCards v-if="photographyModules.length > 0" :modules="photographyModules" mdDisplay="col-md-12"></ModuleCategoryCards>
+            <ModuleCategoryCards v-if="webServicesModules.length > 0" :modules="webServicesModules"></ModuleCategoryCards>
+          </template>
+        </div>
 			</div>
 		</template>
 		<template v-else>
@@ -34,6 +26,7 @@
 	</div>
 </template>
 <script>
+import ModuleCategoryCards from './ModuleCategoryCards.vue'
 export default {
 	created() {
 		// If the user has any roles (or if the user accessing the homepage is logged in)
@@ -44,7 +37,7 @@ export default {
 			})
 		}
 	},
-	components: {},
+	components: {ModuleCategoryCards},
 	props: {
 		username: {
 			type: String,
@@ -64,7 +57,8 @@ export default {
 						"The campus map application contains all points of interest at EMU. These items are displayed at emich.edu/maps.",
 					buttonText: "Open Application",
 					buttonLink: "/map/items",
-					display: false
+					display: false,
+          category: 'Map & Directory'
 				},
 				redirect: {
 					title: "Redirect Application",
@@ -72,7 +66,8 @@ export default {
 						"The redirect application is responsible for setting up 301 redirects and vanity URLs for all EMU pages.",
 					buttonText: "Manage Redirects",
 					buttonLink: "/redirects",
-					display: false
+					display: false,
+          category: 'Web Services'
 				},
 				programs: {
 					title: "Catalog Programs Manager",
@@ -80,7 +75,8 @@ export default {
 						"The catalog programs manager allows for the editing of program names, websites, etc., from the Acalog course catalog.",
 					buttonText: "Manage Programs",
 					buttonLink: "/programs",
-					display: false
+					display: false,
+          category: 'Web Services'
 				},
 				directory: {
 					// Added July 2025
@@ -89,7 +85,8 @@ export default {
 						"The department directory application manages all department information for the university directory.",
 					buttonText: "Manage Departments",
 					buttonLink: "/directory",
-					display: false
+					display: false,
+          category: 'Map & Directory'
 				},
 				photorequests: {
 					// Added July 2025
@@ -98,7 +95,8 @@ export default {
 						"The photo requests application handles photography and headshot requests that are submitted on the EMU website.",
 					buttonText: "See Requests",
 					buttonLink: "/photorequests",
-					display: false
+					display: false,
+          category: 'Photography'
 				},
 				links: {
 					// Added Sept. 2024
@@ -107,16 +105,8 @@ export default {
 						"A list of links to admin panels and front-ends for various external applications.",
 					buttonText: "See Apps",
 					buttonLink: "/applinks",
-					display: true // No permissions required for this module; it's just a list of links
-				},
-				crimelog: {
-					// Added June 2025
-					title: "DPS Crime Log",
-					description:
-						'This application allows DPS staff to upload the Daily Crime Log CSV file for display on the <a href="https://www.emich.edu/police/crime-alerts-stats/daily-crime-log.php" target="_blank">EMU Police website</a>.',
-					buttonText: "DPS Crime Log Upload",
-					buttonLink: "/crimelog",
-					display: true
+					display: true, // No permissions required for this module; it's just a list of links
+          category: 'Web Services'
 				},
         emergency: {
           // Added Sept. 2025
@@ -125,12 +115,59 @@ export default {
               "The emergency banner application manages the emergency banner and page for EMU websites.",
           buttonText: "Manage Banner",
           buttonLink: "/emergency",
-          display: false
+          display: false,
+          category: 'Campus Safety'
         },
+				crimelog: {
+					// Added June 2025
+					title: "DPS Crime Log",
+					description:
+						'This application allows DPS staff to upload the Daily Crime Log CSV file for display on the <a href="https://www.emich.edu/police/crime-alerts-stats/daily-crime-log.php" target="_blank">EMU Police website</a>.',
+					buttonText: "DPS Crime Log Upload",
+					buttonLink: "/crimelog",
+					display: false,
+          category: 'Campus Safety'
+				},
 			}
 		}
 	},
 	computed: {
+    campusSafetyModules: function() {
+      let modules = []
+      for (let module in this.userModules) {
+        if (this.userModules[module].category === 'Campus Safety' && this.userModules[module].display === true) {
+          modules.push(this.userModules[module])
+        }
+      }
+      return modules
+    },
+    webServicesModules: function() {
+      let modules = []
+      for (let module in this.userModules) {
+        if (this.userModules[module].category === 'Web Services' && this.userModules[module].display === true) {
+          modules.push(this.userModules[module])
+        }
+      }
+      return modules
+    },
+    photographyModules: function() {
+      let modules = []
+      for (let module in this.userModules) {
+        if (this.userModules[module].category === 'Photography' && this.userModules[module].display === true) {
+          modules.push(this.userModules[module])
+        }
+      }
+      return modules
+    },
+    mapAndDirectoryModules: function() {
+      let modules = []
+      for (let module in this.userModules) {
+        if (this.userModules[module].category === 'Map & Directory' && this.userModules[module].display === true) {
+          modules.push(this.userModules[module])
+        }
+      }
+      return modules
+    },
 		// Does this user have permission to access any applications?
 		userHasModules: function () {
 			let hasModules = false
@@ -192,14 +229,7 @@ export default {
 			}
 		}
 	},
-	filters: {},
-	watch: {}
 }
 </script>
 <style lang="scss">
-@import "../../../assets/css/variables";
-.emergency-card {
-  background: $danger-emu !important;
-  color: white !important;
-}
 </style>
---
+++ assets/js/components/ModuleCategoryCards.vue
@@ -0,0 +1,55 @@
+<template>
+	<div>
+    <div class="row">
+      <div class="col-sm-12">
+        <h3 class="my-4">{{ modules[0].category }}</h3>
+      </div>
+      <template v-for="(module, i) in modules" :key="`${module.category}-mod-${i}`">
+        <div
+            class="card"
+            :class="`${smDisplay} ${mdDisplay}`"
+        >
+          <div class="card-body">
+            <h5 class="card-title">{{ module.title }}</h5>
+            <p class="card-text"><span v-html="module.description"></span></p>
+            <a :href="module.buttonLink" class="btn btn-primary">{{
+                module.buttonText
+              }}</a>
+          </div>
+        </div>
+      </template>
+    </div>
+	</div>
+</template>
+<script>
+export default {
+	created() {},
+	components: {},
+	props: {
+		modules: {
+			type: Array,
+			required: false
+		},
+    mdDisplay: {
+      type: String,
+      default: 'col-md-4'
+    },
+    smDisplay: {
+      type: String,
+      default: 'col-sm-6'
+    }
+	},
+	data: function () {
+		return {}
+	},
+	computed: {},
+	methods: {},
+}
+</script>
+<style lang="scss">
+@import "../../../assets/css/variables";
+//.emergency-card {
+//  background: $danger-emu !important;
+//  color: white !important;
+//}
+</style>
